545a2ac771f7cbfb7e558f5e64c3c826ff0991a2
mahmudremal <bdcodehaxor@gmail.com>
2022-09-15 02:58:34 +0600

    first commit

---
+++ assets/build/assets.php
@@ -0,0 +1,1 @@
+<?php return array('js/users.js' => array('dependencies' => array('wp-polyfill'), 'version' => '6f4f5343e010e1c354c5b294cf7e22b7'));
---
+++ assets/src/js/users.js
@@ -0,0 +1,8 @@
+import 'regenerator-runtime/runtime';
+
+
+// script loader
+import './content/buyerRequestsDetails';
+
+// Css loader
+import "../sass/users.scss";
